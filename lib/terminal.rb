833e82c12014587e4e4ee02e376631946728b12a
kei <kei@omise.co>
2018-03-09 16:21:55 +0900

    terminal module newly added

---
+++ lib/terminal.rb
@@ -0,0 +1,37 @@
+require 'io/console'
+
+class Terminal
+  ESCAPE_KEY     = "\e"
+  UP_ARR_KEY     = "\e[A"
+  DOWN_ARR_KEY   = "\e[B"
+  RETURN_KEY     = "\r"
+  RIGHT_ARR_KEY  = "\e[C"
+  LEFT_ARR_KEY   = "\e[D"
+  BACK_SPACE_KEY = "\177"
+  CONTROL_C      = "\u0003"
+ 
+  class << self
+    def get_arrow_input(&block)
+      c = read_char
+
+      yield(c) # do something with c
+
+      c == CONTROL_C || get_arrow_input(&block)
+    end
+   
+    def read_char
+      STDIN.echo = false
+      STDIN.raw!
+    
+      input = STDIN.getc.chr
+      if input == "\e" then
+        input << STDIN.read_nonblock(3) rescue nil
+        input << STDIN.read_nonblock(2) rescue nil
+      end
+    ensure
+      STDIN.echo = true
+      STDIN.cooked!
+      return input
+    end
+  end  
+end
