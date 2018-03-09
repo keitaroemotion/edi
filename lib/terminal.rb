167d2314a62a4663dfdc497a32f73b3ead6e5e8c
kei <kei@omise.co>
2018-03-09 18:10:21 +0900

    XXX

--- lib/terminal.rb
+++ lib/terminal.rb
@@ -39,5 +39,33 @@ class Terminal
       STDIN.cooked!
       return input
     end
+   
+    def enlist_files(files)
+      pointer_idx = 0
+      enlist_to_terminal(files, 0, true)
+      
+      Terminal.get_arrow_input do |c|
+        if c == Terminal::DOWN_ARR_KEY || c == Terminal::UP_ARR_KEY
+          if c == Terminal::DOWN_ARR_KEY
+            pointer_idx = pointer_idx + 1
+          else
+            pointer_idx = pointer_idx - 1
+          end
+          print "------------------------------------\n"
+          enlist_to_terminal(files, pointer_idx)
+        end
+      end
+    end  
+
+    private
+
+    def enlist_to_terminal(files, pointer_idx, init = false)
+      pointer_idx = files.size - 1 if pointer_idx >= files.size
+      pointer_idx == 0             if pointer_idx < 0
+    
+      files.each_with_index do |file, i|
+        puts(i == pointer_idx ? file.bold : file)
+      end
+    end
   end  
 end
